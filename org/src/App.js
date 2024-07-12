5c9830dc9d6f38d306098b661228275dbf363860
Ana Castello <ana99.castello@gmail.com>
2024-07-11 21:31:26 -0300

    formulario

---
+++ org/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Componentes/Header/header';
+import Formulario from './Componentes/Formulario/Formulario';
+
+console.log(Header)
+function App() {
+  return (
+    <div>
+      <Header />
+      <Formulario />
+    </div>
+  );
+}
+
+export default App;
---
+++ org/src/Componentes/CampoTexto/index.js
@@ -0,0 +1,9 @@
+import "./CampoTexto.css"
+const CampoTexto=()=>{
+    return <div className="campo-texto">
+        <label>Nombre</label>
+        <input placeholder="Ingresar nombre"/>
+    </div>
+}
+
+export default CampoTexto
